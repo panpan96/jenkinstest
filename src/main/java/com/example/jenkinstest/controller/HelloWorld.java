9965e61a83f3073cff306225b30c8138cf7f3748
panpan96 <1169009524@qq.com>
2018-09-29 15:25:07 +0800

    test

---
+++ src/main/java/com/example/jenkinstest/controller/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.example.jenkinstest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther zhoutf
+ * @Date 2018/9/29 15:06
+ * @Description
+ */
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/")
+    public String getHelloWorld()
+    {
+        return "hello world jenkins!!";
+    }
+}
